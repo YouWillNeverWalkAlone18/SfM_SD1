e31a95c38ffe1980152b0f6094ec78b9bf94bf55
YouWillNeverWalkAlone18 <muyejoong@gmail.com>
2025-06-03 16:40:17 +0900

    Update SfM_SD.js

--- SfM_SD.js
+++ SfM_SD.js
@@ -68,9 +68,9 @@ let instructions = {
         <p>この研究は、□□□□□□□の研究を行うことで○○○○の仕組みを解明することを目的としています。</p>
         <p>実験開始前にこの説明書をお読みいただき、ご協力いただける場合には、任意のキーを入力して、実験を開始します。</p>
       </div>
-      <img src="https://raw.githubusercontent.com/YouWillNeverWalkAlone18/main/CwCCw2.png"
+      <img src="https://raw.githubusercontent.com/YouWillNeverWalkAlone18/SfM_SD1/main/CwCCw2.png"
      alt="回転方向の例"
-     style="display:block; margin: 40px; width: 400px;">
+     style="margin: 40px; width: 400px;">
     </div>
   `,
 };
